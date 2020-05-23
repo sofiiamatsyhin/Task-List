1c435d3cb3447e566beed822e3e9b2efe58eb49d
Sofiia <matsyhin@Sofiias-MacBook-Air.local>
2020-05-24 01:17:51 +0300

    Delete and filter

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const clearBtn = document.querySelector('.clear-task');
 callEventListener();
 function callEventListener() {
     form.addEventListener('submit', addTask);
+    taskList.addEventListener('click', removeItem);
+    clearBtn.addEventListener('click', removeAllItems);
+    filter.addEventListener('keyup', filterTasks);
 }
 function addTask(e) {
     if(taskInput.value === '') {
@@ -25,4 +28,27 @@ function addTask(e) {
         taskInput.value = '';
     }
     e.preventDefault();
+}
+function removeItem(e) {
+    if(e.target.parentElement.classList.contains('delete')) {
+        if(confirm('Are you sure?')) {
+          e.target.parentElement.parentElement.remove();  
+        }
+    }
+}
+function removeAllItems() {
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) !== -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
